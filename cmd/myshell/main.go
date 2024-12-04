558669ba1d0710f17fba31377c0e5152c810f218
Edward Ruzek <edadiddles@hotmail.com>
2024-12-04 18:59:17 -0500

    refactoring out of the repl function and providing exit cmd with tatus code

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,29 +5,48 @@ import (
 	"fmt"
 	"os"
     "strings"
+    "slices"
+    "strconv"
 )
 
-func main() {
-    for {
-        repl()
-    }
+var CMD_LIST = []string {
+    "exit",
 }
 
-func repl() {
-    fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-    input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-    if err != nil {
-        panic(err)
+func main() {
+    status := 0
+    for {
+        fmt.Fprint(os.Stdout, "$ ")
+
+        // Wait for user input
+        input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+        if err != nil {
+            panic(err)
+        }
+
+        input = strings.TrimSpace(input)
+        splt_input := strings.Split(input, " ")
+
+        if len(splt_input) == 0 {
+           return 
+        }
+        if !is_valid_cmd(splt_input[0]) {
+            fmt.Fprintf(os.Stdout, "%s: command not found\n", splt_input[0])
+        }
+
+        if string(splt_input[0]) == "exit" {
+            status, err = strconv.Atoi(splt_input[1])
+            fmt.Fprintf(os.Stdout, "Exiting with status %d\n", status)
+            break
+        }
     }
 
-    input = strings.TrimSpace(input)
-    if !is_valid_cmd(input) {
-        fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+    if status != 0 {
+        panic("program ended with non-zero status")
     }
 }
 
 func is_valid_cmd(cmd string) bool {
-    return false
+    return slices.Contains(CMD_LIST, cmd)
 }
