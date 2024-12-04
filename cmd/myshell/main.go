f24a4bc5606b6d714e98a12f91d85b56dff7af1e
Edward Ruzek <edadiddles@hotmail.com>
2024-12-04 18:12:27 -0500

    adding input cmd validation

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,15 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+    "strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
-	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+    input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+    if err != nil {
+        panic(err)
+    }
+
+    input = strings.TrimSpace(input)
+    if !is_valid_cmd(input) {
+        fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+    } 
+}
+
+func is_valid_cmd(cmd string) bool {
+    return false
 }
